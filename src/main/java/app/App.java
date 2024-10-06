161616bfe91fe0976f20fe6a2c645174221a8025
hienonichan <hvu6582@gmail.com>
2024-10-06 22:02:34 +0700

    complete Student repository

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -11,7 +11,6 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage arg0) {
         FXMLResolver.getInstance().setUp("/view", arg0, 1280, 720);
