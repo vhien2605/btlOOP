f8e77a88b0a4be701baadab635f480cfd258290d
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-11-16 23:47:08 +0700

    add javadoc

--- src/main/java/app/config/SetUpApp.java
+++ src/main/java/app/config/SetUpApp.java
@@ -9,12 +9,25 @@ import app.service.authService.AuthenticationService;
 import app.service.authService.SessionService;
 import app.service.mainService.UserService;
 
+/**
+ * This class is responsible for setting up and redirecting the application
+ * based on the session state.
+ * It determines the user's role (ADMIN or USER) and redirects to the
+ * appropriate page.
+ */
 public class SetUpApp {
 
     private static SessionService sessionService = new SessionService();
 
+    /**
+     * This method verifies the session and redirects the user to the appropriate
+     * page
+     * based on their role. If the session is invalid, the user is redirected to the
+     * login page.
+     */
     public static void setUpApp() {
         try {
+            // Retrieve the current session data
             String sessionData = sessionService.verifySession();
             String role = sessionData.split(" ")[1];
             if (role.equals("ADMIN")) {
@@ -23,14 +36,22 @@ public class SetUpApp {
                 setUpAppUser();
             }
         } catch (SessionException e) {
+            // If there is an issue with the session, redirect to the login page
             FXMLResolver.getInstance().renderScene("auth/login");
         }
     }
 
+    /**
+     * Redirects the user to the admin page.
+     */
     private static void setUpAppAdmin() {
         FXMLResolver.getInstance().renderScene("admin/homeTab/home_tab");
     }
 
+    /**
+     * Redirects the user to the user page.
+     * It also sets up the authentication service for the user page.
+     */
     private static void setUpAppUser() {
         MainHomePageController
                 .getAuthService(new AuthenticationService(new SessionService(), new UserService(new UserRepository())));
