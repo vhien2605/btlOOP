fefc1fdeb54966ea3644f0351fcaebb6e49e40c3
hienonichan <hvu6582@gmail.com>
2024-11-17 12:32:36 +0700

    demo view setting ,relates to #93

--- src/main/java/app/controller/admin/SettingTab/MainSettingController.java
+++ src/main/java/app/controller/admin/SettingTab/MainSettingController.java
@@ -1,12 +1,22 @@
 package app.controller.admin.SettingTab;
 
 import app.controller.BaseController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class MainSettingController implements BaseController {
+
+    @FXML
+    Label nameField;
+    @FXML
+    Label usernameField;
+    @FXML
+    Label addressField;
+    @FXML
+    Label phoneField;
 
     @Override
     public void initialize() {
 
     }
-
 }
