7547803c25b3a4870befb42d8a68ffc642751e27
nhuhieu193 <mama82bells@gmail.com>
2024-11-24 00:59:39 +0700

    re-design user

--- src/main/java/app/controller/user/BookLoan/BookLoanController.java
+++ src/main/java/app/controller/user/BookLoan/BookLoanController.java
@@ -4,6 +4,11 @@ import app.config.ViewConfig.FXMLResolver;
 import app.controller.BaseController;
 import app.domain.Book;
 import app.domain.DTO.SurfaceUserDTO;
+import app.exception.auth.SessionException;
+import app.repository.UserRepository;
+import app.service.authService.AuthenticationService;
+import app.service.authService.SessionService;
+import app.service.mainService.UserService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -43,9 +48,36 @@ public class BookLoanController implements BaseController {
     @FXML
     private Button backButton;
 
-    // public BookLoanController(Book book, SurfaceUserDTO user, String status) {
+    private Book book;
 
-    // }
+    private SurfaceUserDTO user;
+
+    private String status;
+
+    private AuthenticationService authService;
+
+    public void LoadBookLoanWithBookAndStatus(Book book, String status) {
+        this.book = book;
+        this.status = status;
+        RenderUserInfo();
+        RenderBookInfo();
+    }
+
+    private void RenderBookInfo() {
+        bookISBNTextField.setText(book.getId());
+        bookNameTextField.setText(book.getName());
+        bookAuthorTextField.setText(book.getAuthor());
+        bookPublisherTextField.setText(book.getBookPublisher());
+        bookCategoryTextField.setText(book.getCategory());
+    }
+
+    private void RenderUserInfo() {
+        userIdTextField.setText(user.getId());
+        fullNameTextField.setText(user.getName());
+        phoneNumberTextField.setText(user.getPhoneNumber());
+        emailTextField.setText(user.getEmail());
+        addressTextField.setText(user.getAddress());
+    }
 
     public void handleEvent(ActionEvent e) {
         if (e.getSource() == backButton) {
@@ -56,7 +88,16 @@ public class BookLoanController implements BaseController {
     @Override
     public void initialize() {
         // TODO Auto-generated method stub
-        
+        authService = new AuthenticationService(new SessionService(), new UserService(new UserRepository()));
+        getCurrentUserInfo();
+    }
+
+    private void getCurrentUserInfo() {
+        try {
+            user = authService.getCurrentUser();
+        } catch (SessionException exception) {
+            FXMLResolver.getInstance().renderScene("auth/login");
+        }
     }
     
 }
