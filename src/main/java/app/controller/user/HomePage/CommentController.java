32c4524ca8b1e93ec30d48b3ab0b57dbeef1f37f
nhuhieu193 <mama82bells@gmail.com>
2024-11-27 14:07:17 +0700

    tmp commit

---
+++ src/main/java/app/controller/user/HomePage/CommentController.java
@@ -0,0 +1,48 @@
+package app.controller.user.HomePage;
+
+
+import app.controller.BaseController;
+import app.domain.Book;
+import app.domain.Comment;
+import app.domain.User;
+import app.repository.BookRepository;
+import app.repository.UserRepository;
+import app.service.authService.AuthenticationService;
+import app.service.mainService.BookService;
+import app.service.mainService.UserService;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class CommentController implements BaseController{
+    @FXML
+    private TextArea commentTextArea;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private Label commentDateLabel;
+
+    private UserService userService;
+
+    private BookService bookService;
+
+    public void loadComment(Comment comment) {
+        String userID = comment.getStudentId();
+        User user = userService.findById(userID);
+        String bookID = comment.getBookId();
+        Book book = bookService.findByISBN(bookID);
+        
+        usernameLabel.setText(user.getName());
+        commentTextArea.setText(comment.getInformation());
+        commentDateLabel.setText(comment.getDate());
+    }
+
+    @Override
+    public void initialize() {
+        bookService = new BookService(new BookRepository());
+        userService = new UserService(new UserRepository());
+    }
+    
+}
